c269ed0dcdd6ce53e46e3ff0dc5e70675c2fecc8
zwiebert <bertw@online.de>
1999-12-18 13:29:36 +0000

    simple working version with dial-button, cost-counter, dynamic pfg/minute (for long connects only!) field

--- dialup_manager.pl
+++ dialup_manager.pl
@@ -1,17 +1,15 @@
 #! /usr/local/bin/perl -w
 
+my $db_ready = 0;
+
 my $isp_curr= defined $ARGV[0] ? $ARGV[0] : '';
 
 use strict;
 use Time::Local;
-use IO::Handle;
-STDOUT->autoflush();
 
 #-- begin library
 my %month_map=(Jan => 0, Feb => 1, Mar => 2, Apr => 3, May => 4, Jun => 5,
 	       Jul => 6, Aug => 7, Sep => 8, Oct => 9, Nov => 10, Dec => 11);
-
-my $time_start = time ();
 
 
 sub parsetime ($) {
@@ -41,7 +39,7 @@ sub tarif ($$) {
     my $einwahlstrafe=0;	# Manche Provider berechnen eine Verbindungsgebuehr (T-Online jetzt nicht mehr)
     my @date=parsetime (localtime ($date_arg));
 
-    if ($isp =~ /^NGI/) {
+    if ($isp =~ /^NGI/ or $isp =~ /^NGI_SH/) {
 	$pfg=4.89 / 60;
 	$takt=1;
 	$offset=1;
@@ -101,8 +99,10 @@ sub tarif ($$) {
 	    $takt=4 * 60;
 	} elsif ($date[0] eq "Sat" or $date[0] eq "Sun") {
 	    $takt=2.5 * 60;
-	} else {
+	} elsif ($date[3] >= 18 and $date[3] < 21) {
 	    $takt=2.5 * 60;
+	} else {
+	    $takt=1.5 * 60;
 	}
 	if (!($date[0] eq "Sat" or $date[0] eq "Sun")
 	    && !($date[3] >= 20 or $date[3] < 8)) {
@@ -118,87 +118,220 @@ sub tarif ($$) {
     ($pfg, $takt, $offset, $einwahlstrafe, $isp_pfg, $isp_takt);
 }
 
+sub calc_price ( $$$ ) {
+    my ($isp, $start_time, $duration) = @_;
+    my @tar = tarif ($isp, $start_time);
+    my $result = $tar[0] * ($duration / $tar[1]);
+    $result +=  $tar[4] * ($duration / $tar[5]);
+    $result;
+}
 #- end library
 
-my @sum;
-my @time_last = (0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
-my @time_last_isp = (0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
-my @isps = ("KNUUT", "KNUUT_MCI", "KNUUT_0800", "NIKOMA", "NGI"); 
+my $flag_online = 0;
+my $flag_dialing = 0;
+my $flag_init = 0;
+my %records;
+my ($offs_sum, $offs_time_last, $offs_time_last_isp) = (0, 1, 2, 3, 4, 5, 6, 7, 8, 9);
+#my @sum;
+#my @time_last = (0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
+#my @time_last_isp = (0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
+my @isps = ("KNUUT", "KNUUT_MCI", "KNUUT_0800", "NIKOMA", "NGI", "NGI_SH"); 
+
+my $time_start = 0;
+
+sub init () {
+    $time_start = time ();
+    foreach my $isp (@isps) {
+	$records{$isp}=[0, 0, 0];
+    }
+    $flag_init = 1;
+}
+
+sub cb_disconnect ();
+
+sub online () {
+    ($flag_online and -S '/tmp/.ppp' and qx(/usr/sbin/pppctl 2>/dev/null -p '' -v '/tmp/.ppp' quit) =~ /^PPP/);
+}
+
+sub check_online () {
+    if (online ()) {
+	if ($flag_init) {
+	    1;
+	} else {
+	    init ();
+	    2;
+	}
+    } elsif ($flag_online) {
+	$flag_online=0;
+	$flag_init=0;
+	-1;
+    } else {
+	0;
+    }
+}
 
 sub update_sum () {
     my $time_curr = time ();
-    for (my $i=0; $i <= $#isps; $i++) {
-	my @tarif_curr = tarif ($isps[$i], time ());
-	if ($time_last[$i] == 0) {
-	    $sum[$i] = $tarif_curr[3];
-	    $time_last[$i] = $time_start - $tarif_curr[2];
+    foreach my $isp (@isps) {
+	my $rec = $records{$isp};
+	my @tarif_curr = tarif ($isp, time ());
+	if ($$rec[$offs_time_last] == 0) {
+	    $$rec[$offs_sum] = $tarif_curr[3];
+	    $$rec[$offs_time_last] = $time_start - $tarif_curr[2];
 	}
-	my $units_curr = ($time_curr - $time_last[$i]) / $tarif_curr[1];
-	while ($time_last[$i] < $time_curr) {
-	    $time_last[$i] += $tarif_curr[1];
-	    $sum[$i] += $tarif_curr[0];
+	my $units_curr = ($time_curr - $$rec[$offs_time_last]) / $tarif_curr[1];
+	while ($$rec[$offs_time_last] < $time_curr) {
+	    $$rec[$offs_time_last] += $tarif_curr[1];
+	    $$rec[$offs_sum] += $tarif_curr[0];
 	}
-	if ($time_last_isp[$i] == 0) {
-	    $time_last_isp[$i] = $time_start;
+	if ($$rec[$offs_time_last_isp] == 0) {
+	    $$rec[$offs_time_last_isp] = $time_start;
 	}
-	$units_curr = ($time_curr - $time_last_isp[$i]) / $tarif_curr[5];
-	while ($time_last_isp[$i] < $time_curr) {
-	    $time_last_isp[$i] += $tarif_curr[5];
-	    $sum[$i] += $tarif_curr[4];
+	$units_curr = ($time_curr - $$rec[$offs_time_last_isp]) / $tarif_curr[5];
+	while ($$rec[$offs_time_last_isp] < $time_curr) {
+	    $$rec[$offs_time_last_isp] += $tarif_curr[5];
+	    $$rec[$offs_sum] += $tarif_curr[4];
 	}
     }
 }
 
-
-while (0) {
-    sleep (1);
-    update_sum ();
-    printf "\r";
-    for (my $i=0; $i <= $#isps; $i++) {
-	printf " %s: %.3f DM |", $isps[$i], $sum[$i] / 100;
-    }
-}
 
 ## Tk-GUI
 use Tk;
 my $main;
 my @entries;
+my %labels;
+my $disconnect_button;
 
-sub update_gui () {
-    my $i=0;
-    my $cheapest=999999;
-    my $most_expensive=0;
-    update_sum ();
-    foreach my $i (@sum) {
-	$cheapest = $i if $cheapest > $i;
-	$most_expensive = $i if $most_expensive < $i;
-    }
-    foreach my $entry (@entries) {
-	my $price = $sum[$i];
-	$entry->delete ('1.0', 'end');
-	$entry->insert('1.0', sprintf ("%02.3f DM", $price / 100));
-	my $bg_color = (($cheapest == $price) ? 'Green'
-			: (($most_expensive == $price) ? 'OrangeRed'
-			   : 'Yellow'));
-	$entry->configure (-background => $bg_color);
+my %widgets;
+my ($offs_record, $offs_isp_widget, $offs_sum_widget, $offs_min_price_widget) = (0, 1, 2, 3, 4, 5, 6, 7, 8, 9);
 
-	++$i;
+
+sub update_gui_offline () {
+    foreach my $isp (@isps) {
+	my $label =  $labels{$isp};
+	my $bg_color = ($isp ne $isp_curr) ? $label->parent->cget('-background') : 'Grey';
+	$label->configure(-background => $bg_color);
     }
 }
+sub update_gui_online () {
+    foreach my $isp (@isps) {
+	my $label =  $labels{$isp};
+	my $bg_color = ($isp ne $isp_curr) ? $label->parent->cget('-background') : 'Cyan';
+	$label->configure(-background => $bg_color);
+    }
+}
+
+
+sub update_gui () {
+    my $ready = check_online ();
+    my $curr_time = time();
+    my $cheapest=999999;
+    my $most_expensive=0;
+
+    update_gui_offline if ($ready < 0);
+
+    if ($ready > 0) {
+	$disconnect_button->configure(-state => 'active') if $disconnect_button->cget('-state') eq 'disabled';
+	my $i=0;
+	update_sum ();
+	foreach my $rec (%records) {
+	    next unless ref $rec;
+	    my $i = $$rec[$offs_sum];
+	    $cheapest = $i if $cheapest > $i;
+	    $most_expensive = $i if $most_expensive < $i;
+	}
+    } else {
+	$disconnect_button->configure(-state => 'disabled');
+    }
+
+    my $isp;
+    foreach my $wid (%widgets) {
+	if (! ref $wid) {
+	    $isp = $wid;
+	    next;
+	}
+	my $rec=$records{$isp};
+	if ($ready > 0) {
+	    my $price = $$rec[$offs_sum];
+	    my $entry=$$wid[$offs_sum_widget];
+	    $entry->delete ('1.0', 'end');
+	    $entry->insert('1.0', sprintf ("%4.2f Pfg", $price));
+	    my $bg_color = (($cheapest == $price) ? 'Green'
+			    : (($most_expensive == $price) ? 'OrangeRed'
+			       : 'Yellow'));
+	    $entry->configure (-background => $bg_color);
+	}
+
+	{			# minute price
+	    my $widget=$$wid[$offs_min_price_widget];
+	    $widget->delete ('1.0', 'end');
+	    $widget->insert('1.0', sprintf ("%.2f", calc_price ($isp, $curr_time, 60)));
+	}
+    }
+}
+
+sub cb_disconnect () {
+    if ($db_ready or -S '/tmp/.ppp') {
+	qx(/usr/sbin/pppctl /tmp/.ppp close);
+	$flag_online=0;
+	$flag_init=0;
+	# remove highlight on isp labels
+	foreach my $isp (@isps) {
+	    my $label =  $labels{$isp};
+	    my $bg_color = ($isp ne $isp_curr) ? $label->parent->cget('-background') : 'Grey';
+	    $label->configure(-background => $bg_color);
+ 	}
+    }
+    0;
+}
+
+my %isp_cmd_map = (KNUUT => 'connect-isp-030.pl', KNUUT_MCI =>  'connect-isp.pl', KNUUT_0800 => 'connect-isp-0800.pl',
+		   NIKOMA => 'connect-isp-nikoma.pl', NGI => 'connect-isp-ngi.pl', NGI_SH => 'connect-isp-ngi-sh.pl');
+my %isp_label_map = (KNUUT => 'Knuut-030', KNUUT_MCI =>  'Knuut-MCI',  KNUUT_0800 => 'Knuut-0800',
+		     NIKOMA => 'NIKOMA', NGI => 'NGI', NGI_SH => 'NGI Short');
+sub cb_dialup2 ( $ ) {
+    my ($isp) = @_;
+    my $cmd = "/root/bin/pc/" . $ENV{'USER'} . "/" . $isp_cmd_map{$isp} . '&';
+    qx/$cmd/;
+    $flag_online = 1; # FIXME
+    $isp_curr = $isp;
+    update_gui_online () if (check_online () > 0);
+}
+
+sub cb_dialup ( $ ) {
+    my ($isp) = @_;
+    cb_disconnect ();
+    print $isp . "\n";
+    cb_dialup2 ($isp);
+}
+
 sub make_gui () {
     $main = MainWindow->new;
+    $main->appname('dialupManager');
     foreach my $isp (@isps) {
 	my $frame = $main->Frame;
-	my $label = $frame->Label(-text => $isp);
-	my $text = $frame->ROText(-height => 1, -width => 10);
+	my $button = $frame->Button(-text => 'Verbinden', -command => sub{cb_dialup ($isp)});
+#	$button->after(1, sub{cb_dialup2 ($isp)});
+	my $label = $frame->Button(-text => (exists $isp_label_map{$isp} ? $isp_label_map{$isp} : $isp),
+				   -command => sub{cb_dialup ($isp)});
+	my $text = $frame->ROText(-height => 1, -width => 12);
+	my $min_price = $frame->ROText(-height => 1, -width => 6);
 
 	$label->configure(-background => 'Cyan') if ($isp eq $isp_curr);
 
 	$frame->pack(-expand => 1, -fill => 'x');
-	$label->pack(-side => 'left');
+#	$button->pack(-side => 'left');
+	$label->pack(-expand => 1, -fill => 'x', -side => 'left');
+	$min_price->pack(-side => 'right');
 	$text->pack(-side => 'right');
 	$entries[$#entries+1] = $text;
+	$labels{$isp} = $label;
+	$widgets{$isp} = [0, $label, $text, $min_price];
     }
+    my $button = $main->Button(-text => 'Trennen', -command => sub{cb_disconnect});
+    $button->pack(-expand => 1, -fill => 'x');
+    $disconnect_button=$button;
     $main->repeat (1000, sub{update_gui()});
 }
 
