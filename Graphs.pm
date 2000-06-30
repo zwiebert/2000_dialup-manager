8513c4d18b6534e6afd376b0b095265ff9473d27
zwiebert <bertw@online.de>
2000-06-30 04:30:15 +0000

    * wrong calculating of y values in extend_graphs() fixed

--- Graphs.pm
+++ Graphs.pm
@@ -7,7 +7,7 @@ BEGIN {
     # set the version for version checking
     $VERSION     = 1.00;
     # if using RCS/CVS, this may be preferred
-    $VERSION = do { my @r = (q$Revision: 1.1.1.1.2.1 $ =~ /\d+/g); sprintf "%d."."%02d" x $#r, @r }; # must be all one line, for MakeMaker
+    $VERSION = do { my @r = (q$Revision: 1.1 $ =~ /\d+/g); sprintf "%d."."%02d" x $#r, @r }; # must be all one line, for MakeMaker
 
     @ISA         = qw(Exporter);
     @EXPORT      = qw(&add_graphs &steigung &extend_graphs);
@@ -43,20 +43,22 @@ sub steigung ( $ ) {
 	my $d = ($d1==0) ? 0 : ($g[$i+3] - $g[$i+1]) / $d1;
 	$result[$#result+1]=$d;
     }
-    $result[$#result+1] = $result[$#result]; # FIXME?
     @result;
+}
+
+sub dump_list ( $$ ) {
+    my ($tag, $list) = @_;
+    print "$tag=(";
+    foreach my $i (@$list) {  printf "%.2f, ", $i; }
+    print ")\n";
 }
 
 sub extend_graphs ($$) {
     my ($rg1, $rg2) = @_;
     die unless ref ($rg1) and ref ($rg2);
     if (1) {
-	print "(";
-	foreach my $i (@$rg1) { print " $i,"; }
-	print ")\n";
-	print "(";
-	foreach my $i (@$rg2) { print " $i,"; }
-	print ")\n";
+	dump_list ("rg1", $rg1);
+	dump_list ("rg2", $rg2);
     }
     my @all_x=();
     {
@@ -84,40 +86,44 @@ sub extend_graphs ($$) {
     }
     my @ng1 = ();
     my @ng2 = ();
-    my @st1 = steigung ($rg1);
-    my @st2 = steigung ($rg2);
 
     {
+	my @st1 = steigung ($rg1);
 	my ($ii, $iii, $iiii) = (0,0,0,0,0,0);
 	for (my $i=0; $i <= $#all_x; $i++) {
-	    $ng1[$#ng1+1] = $all_x[$i];
+	    $ng1[$#ng1+1] = $all_x[$i];  # copy x value
 	    if ($$rg1[$ii*2] == $all_x[$i]) {
-		$ng1[$#ng1+1] = $$rg1[$ii*2+1];
+		$ng1[$#ng1+1] = $$rg1[$ii*2+1]; # copy y value (no need for calculate)
 		$ii++;
 	    } else {
-		$ng1[$#ng1+1] =  $all_x[$i] * $st1[$ii];
+		$ng1[$#ng2+1] =  $ng1[$#ng2-1] + (($all_x[$i] - $all_x[$i-1]) * $st1[$ii-1]); # calculate y value
 	    }
 	}
     }
     {
+	my @st2 = steigung ($rg2);
 	my ($ii, $iii, $iiii) = (0,0,0,0,0,0);
 	for (my $i=0; $i <= $#all_x; $i++) {
-	    $ng2[$#ng2+1] = $all_x[$i];
+       	    $ng2[$#ng2+1] = $all_x[$i];  # copy x value
 	    if ($$rg2[$ii*2] == $all_x[$i]) {
-		$ng2[$#ng2+1] = $$rg2[$ii*2+1];
+		$ng2[$#ng2+1] = $$rg2[$ii*2+1]; # copy y value (no need for calculate)
 		$ii++;
 	    } else {
-		$ng2[$#ng2+1] =  $all_x[$i] * $st2[$ii];
+		$ng2[$#ng2+1] =  $ng2[$#ng2-1] + (($all_x[$i] - $all_x[$i-1]) * $st2[$ii-1]); # calculate y value
 	    }
 	}
     }
     \ (@ng1, @ng2);
 }
 
+
 # add the y values of both graphs GRAPH_REF_1 and GRAPH_REF_2
 # Bug: both graphs have to have common values for both first and last x
 sub add_graphs( $$ ) {
     my ($rg1, $rg2) = @_;
+    die unless ($$rg1[0] == $$rg2[0]);
+    die unless ($$rg1[$#$rg1-1] == $$rg2[$#$rg2-1]);
+
     my ($rg1e, $rg2e) = extend_graphs ($rg1, $rg2);
     my @result=();
     for (my $i=0; $i <= $#$rg1e; $i+=2) {
